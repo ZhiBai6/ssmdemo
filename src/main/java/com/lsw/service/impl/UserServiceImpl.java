f285f1d8d2023ef38481e6f2a7577b1bdf6676ee
lsw <15887845958@163.com>
2022-05-02 14:10:43 +0800

    first commit

---
+++ src/main/java/com/lsw/service/impl/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package com.lsw.service.impl;
+
+import com.lsw.mapper.UserMapper;
+import com.lsw.pojo.User;
+import com.lsw.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ *
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    //切记切记:一定会有数据访问层的对象
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public List<User> selectUserPage(String userName, String userSex, int startRow) {
+        return userMapper.selectUserPage(userName, userSex, startRow);
+    }
+
+    @Override
+    public int createUser(User user) {
+        return userMapper.createUser(user);
+    }
+
+    @Override
+    public int deleteUserById(String userId) {
+        return userMapper.deleteUserById(userId);
+    }
+
+    @Override
+    public int getRowCount(String userName, String userSex) {
+        return userMapper.getRowCount(userName, userSex);
+    }
+
+    @Override
+    public int updateByAll(User user) {
+        return userMapper.updateByAll(user);
+    }
+}
